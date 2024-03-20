e496211294fa8ed3533c162c8e424d53a64e2d21
Times0 <45049767+Times0@users.noreply.github.com>
2024-03-20 02:12:01 +0100

    only unix

--- main.py
+++ main.py
@@ -1,21 +1,45 @@
-import time
+import requests
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
-
-options = webdriver.ChromeOptions()
-options.add_argument("--headless")
-
-driver = webdriver.Chrome(options=options)
-
-
-driver.get("https://www.python.org/")
-
-time.sleep(5)
-
-latest_version_element = driver.find_element(
-    By.CSS_SELECTOR,
-    "div.small-widget:nth-child(2) > p:nth-child(3) > a:nth-child(1)",
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.support.ui import WebDriverWait, Select
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import (
+    NoSuchElementException,
+    TimeoutException,
+    UnexpectedAlertPresentException,
 )
-print(f"Latest Python version: {latest_version_element.text}")
-driver.quit()
+
+from pyvirtualdisplay import Display
+
+
+class Browser(object):
+    # initialize with 'chrome' or 'firefox' as the driver
+    def __init__(self, flavor):
+        self.display = Display(visible=0, size=(2880, 1800)).start()
+
+        if flavor.lower() == "chrome":
+            self.driver = webdriver.Chrome()
+        elif flavor.lower() == "firefox":
+            self.driver = webdriver.Firefox()
+        else:
+            raise Exception(
+                "need to specify 'chrome' or firefox' when creating a Browser"
+            )
+
+    def transfer_to_requests(self):
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
+        }
+        self.session = requests.session()
+        self.session.headers.update(headers)
+        for cookie in self.driver.get_cookies():
+            c = {cookie["name"]: cookie["value"]}
+            self.session.cookies.update(c)
+        return self.session
+
+
+# example usage
+b = Browser("chrome")
+b.driver.get("https://www.google.com")
