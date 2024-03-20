82082d5fddb393218193c375dbbef6ef7b42f077
Times0 <45049767+Times0@users.noreply.github.com>
2024-03-20 02:06:46 +0100

    add headless

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
 import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-s = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=s)
+
+options = webdriver.ChromeOptions()
+options.add_argument("--headless")
+
+driver = webdriver.Chrome(options=options)
+
 
 driver.get("https://www.python.org/")
 
