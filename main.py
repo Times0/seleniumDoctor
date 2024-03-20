a48636d89cfa5e4579979fd889cf6f73d0f04b0c
Times0 <45049767+Times0@users.noreply.github.com>
2024-03-20 02:49:43 +0100

    shesh

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
 
 
-options = Options()
-options.add_argument("--headless")
-driver = webdriver.Firefox(options=options)
-
+driver = webdriver.Chrome()
 driver.get("https://www.google.com")
-
-print(driver.title)
 driver.quit()
